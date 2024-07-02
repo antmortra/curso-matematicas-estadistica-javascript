669985575dccac22db0b58f5a9ad3408dec5bbe1
antmortra <antoniomorillotrapero@gmail.com>
2024-07-01 21:04:40 -0400

    guardo avances del proyecto

--- math.js
+++ math.js
@@ -1,3 +1,5 @@
+// CUADRADO
+
 const calcularCuadrado = document.getElementById('calcular-cuadrado');
 const inputCuadrado = document.getElementById('input-cuadrado');
 
@@ -11,38 +13,54 @@ function calculoCuadrado() {
     document.getElementById('areaCuadrado').innerHTML = inputCuadrado.value**2;
 }
 
+// TRIANGULO
 
+const calcularTriangulo = document.getElementById('calcular-triangulo');
+const inputTri1 = document.getElementById('lado1');
+const inputTri2 = document.getElementById('lado2');
+const inputTri3 = document.getElementById('base');
 
+calcularTriangulo.addEventListener('click', calculoTriangulo);
 
+function calculoTriangulo() {
 
+    if (Number(inputTri1.value) === Number(inputTri2.value) && inputTri1.value != inputTri3.value) {
+        document.getElementById('alturaTriangulo').innerHTML = Math.sqrt(Number(inputTri1.value)**2 - (Number(inputTri3.value)**2) / 4).toFixed(2);
+    }
+   
+    document.getElementById('perimetroTriangulo').innerHTML = Number(inputTri1.value) + Number(inputTri2.value) + Number(inputTri3.value);
 
+    const s = (Number(inputTri1.value) + Number(inputTri2.value) + Number(inputTri3.value)) / 2;
 
-
-
-
-
-
-
-// Calcular el perimetro de un cuadrado
-
-function perimetroCuadrado(l) {
-    return l*4;
+    document.getElementById('areaTriangulo').innerHTML = 
+    Math.sqrt(s*(s-Number(inputTri1.value))*(s-Number(inputTri2.value))*(s-Number(inputTri3.value))).toFixed(2);
 }
 
-// Calcular el area de un cuadrado
+// CIRCULO
 
-function areaCuadrado(l) {
-    return l**2;
+const calcularCirculo = document.getElementById('calcular-circulo');
+const inputCirculo = document.getElementById('radio');
+
+calcularCirculo.addEventListener('click', calculoCirculo);
+
+function calculoCirculo() {
+
+    document.getElementById('perimetroCirculo').innerHTML = (2 * Math.PI * Number(inputCirculo.value)).toFixed(2);
+
+    document.getElementById('areaCirculo').innerHTML = (Math.PI * Number(inputCirculo.value)**2).toFixed(2);
+
 }
+    
 
-// Calcular perímetro de un triangulo
 
-function perimetroTriangulo(l1, l2, base) {
-    return l1 + l2 + base;
-}
 
-// Calcular area de un triangulo
 
-function areaTriangulo(base, altura) {
-    return (base * altura) / 2;
-}
+
+
+
+
+
+
+
+
+
