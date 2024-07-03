cde6df94655061277469a2b7589a1aab44050403
antmortra <antoniomorillotrapero@gmail.com>
2024-07-02 23:05:06 -0400

    guardo avances

--- estadistica/mediana/main.js
+++ estadistica/mediana/main.js
@@ -14,21 +14,45 @@ function pushNumber() {
     const inputIn = document.getElementById('inputIn');
     array.push(Number(input.value));
     inputIn.innerHTML = array;
+
     
+
 }
 
+button.addEventListener('click', clear);
+
+function clear() {
+    input.value = '';
+}
+    
 function popNumber() {
     const inputIn = document.getElementById('inputIn');
     array.pop(input.value);
     inputIn.innerHTML = array;
 }
 
-calcular.addEventListener('click', calcularPromedio);
+calcular.addEventListener('click', calcularMediana);
 
-function calcularPromedio() {
+function calcularMediana() {
 
-   const solution1 = array.reduce((a, b) => (a + b)) / (array.length);
-    solution.innerHTML = solution1.toFixed(1);
+   const solution1 = array.sort((a, b) => a - b);
+
+   if (!Number.isInteger(array.length/2)) {
+   
+   const solution2 = solution1[(Math.ceil(array.length/2))-1];
+
+    solution.innerHTML = solution2;
+    }
+    else {
+
+        const solution2 = (array[(array.length/2)-1] + array[(array.length/2)]) / 2 ;
+
+        solution.innerHTML = solution2;
+    }
 
 }
+
+
+
+
 
